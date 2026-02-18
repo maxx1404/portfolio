f4f53abad1c872fb353842747e0d17ccf12da145
maxx1404 <maxsonmathew56@gmail.com>
2026-02-18 21:18:41 +0530

    Added desktop background

---
+++ script.js
@@ -0,0 +1,35 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const icons = document.querySelectorAll('.icon');
+
+    icons.forEach(icon => {
+        let offsetX, offsetY;
+        let isDragging = false;
+
+        icon.addEventListener("mousedown", (e) => {
+            isDragging = true;
+
+            offsetX = e.clientX - icon.offsetLeft;
+            offsetY = e.clientY - icon.offsetTop;
+
+            icon.style.cursor = "grabbing";
+            icon.style.zIndex = 1000;
+            e.preventDefault();
+        });
+
+        document.addEventListener("mousemove", (e) => {
+            if (isDragging) {
+                icon.style.left = e.clientX - offsetX + "px";
+                icon.style.top = e.clientY - offsetY + "px";
+            }
+        });
+
+        document.addEventListener("mouseup", () => {
+            isDragging = false;
+            icon.style.cursor = "grab";
+        });
+    });
+
+});
+
